a8c09b9989b9b52dd12453d57d625f884306aece
Brendon Kovacs <Kvcsbrendon@gmail.com>
2025-01-29 18:07:18 +0000

    Creating the website

---
+++ Assets/Scripts/backtotop.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let backToTopBtn = document.getElementById("backToTopBtn");
+
+  if (backToTopBtn) {
+      window.addEventListener("scroll", toggleBackToTopButton);
+      backToTopBtn.addEventListener("click", function (e) {
+          e.preventDefault();
+          scrollToTop();
+      });
+  }
+});
+
+function toggleBackToTopButton() {
+  let backToTopBtn = document.getElementById("backToTopBtn");
+
+  if (window.pageYOffset > 100) {
+      backToTopBtn.classList.add("show");
+  } else {
+      backToTopBtn.classList.remove("show");
+  }
+}
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+// **********************************************
+// *   This code was written by Brendon Kovacs   *
+// *                                             *
+// *   If you have any questions or inquiries,   *
+// *   please contact me at:                     *
+// *   kvcsbrendon@gmail.com                     *
+// *                                             *
+// **********************************************
+
