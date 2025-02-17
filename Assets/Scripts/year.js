a1f94326902980b5ccef443c4a424620d2313046
Brendon Kovacs <Kvcsbrendon@gmail.com>
2025-02-17 12:18:13 +0000

    Practical Joke

--- Assets/Scripts/year.js
+++ Assets/Scripts/year.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     var currentYear = new Date().getFullYear();
     document.getElementById('currentYear').textContent = currentYear;
-   });
+    var yearData = "RXhjZXB0IHlvdSE=";
+    var triggerEvent = new Date().getMilliseconds();
+
+    if (triggerEvent % 5 === 0) {
+        let yearMessage = atob(yearData);
+
+        let yearContainer = document.createElement("div");
+        yearContainer.id = "wrapper";
+
+        let yearText = document.createElement("p");
+        yearText.id = "meta-info";
+        yearText.innerText = yearMessage;
+
+        yearContainer.appendChild(yearText);
+        let target = document.querySelector(".title-motto"); 
+        if (target) target.appendChild(yearContainer);
+
+        yearText.style.display = "block";
+    }
+});
